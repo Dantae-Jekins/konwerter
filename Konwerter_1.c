803444701b06d83af59ab884b068d930187de0f8
Dantae Jekins <dantae.sona.jekins@gmail.com>
2021-05-06 21:45:32 -0300

    Realloc fully functional

--- Konwerter_1.c
+++ Konwerter_1.c
@@ -4,8 +4,87 @@
 
 //  Declaração das funções (para o compilador não assumir errado);
 //  Suponho que funcionaria se eu colocasse as funções antes de main também.
-int* readInteger();
-char* readChar();
+
+//FUNÇÕES DE LEITURA
+
+int* readInteger() {
+    FILE* file = stdin;
+    int* num;
+    size_t size = 2, count = 0; //tamanho default e contagem para array
+
+    num = (int*) malloc(sizeof(int) * size); // aloca memória ao num de tamanho *num (int) vezes size (2)
+
+    while (EOF != (num[count] = fgetc(file)) && num[count++] != '\n') {
+        /*  EOF != (num[count] = fgetc(stdin))
+            lê o número fgetc(stdin) e o coloca
+            dentro da array num[count], depois
+            confirma que não é EOF.*/
+
+            /*  num[count++] != '\n'
+                compara o número lido com '\n',
+                e depois incrementa count. */
+
+        if (count + 1 == size) {
+            /*  vê se a contagem bateu perto do
+                limite da array, se sim realoca
+                mais espaço para ser utilizado:*/
+
+            num = (int*) realloc(num, sizeof(int) * (size += 1));
+            /*  size+=1 incrementa size e depois faz a
+                operação.*/
+        }
+    }
+    // se a condição do while é falsa, num[count] = '\n' ou EOF.
+
+    // portanto deve ser retornado, com tamanho [count]:
+    // o último digito deve ser -1, para servir de parada na hora de printar
+    num[count] = -1; 
+    return num;
+
+    //retorna um ponteiro apontando para um vetor de inteiros.
+}
+
+char* readChar() {
+    char* num;
+    size_t size = 2, count = 0; //tamanho default e contagem para array
+
+    num = (char*) malloc(sizeof(char) * size); // aloca memória ao num de tamanho *num (int) vezes size (2)
+
+    while (EOF != (num[count] = fgetc(stdin)) && num[count++] != '\n') {
+        /*  EOF != (num[count] = fgetc(stdin))
+            lê o número fgetc(stdin) e o coloca
+            dentro da array num[count], depois
+            confirma que não é EOF.*/
+
+            /*  num[count++] != '\n'
+                compara o número lido com '\n',
+                e depois incrementa count. */
+
+        if (count + 1 == size) {
+            /*  vê se a contagem bateu perto do
+                limite da array, se sim realoca
+                mais espaço para ser utilizado:*/
+
+            num = (char*) realloc(num, sizeof(char) * (size += 1));
+            /*  size+=1 incrementa size e depois faz a
+                operação.*/
+        }
+    }
+
+    // se a condição do while é falsa, num[count] = '\n' ou EOF.
+
+    // portanto deve ser retornado, com tamanho [count]:    
+    num[count+1] = '\0';
+    return num;
+
+    // retorna um ponteiro apontando para um vetor de caracteres.
+}
+
+//FUNÇÕES DE CONVERSÃO
+
+
+
+//FUNÇÕES MATEMÁTICAS
 
 //clear input
 void clearinput(void) {
@@ -14,13 +93,13 @@ void clearinput(void) {
 
 // main
 
-int main() {
+void main() {
     int base=NULL;
-    int* bin=NULL, * oct=NULL, * dec=NULL;
-    char* hex=NULL;
+    int *bin=NULL, *oct=NULL, *dec=NULL;
+    char *hex=NULL;
 
     printf("\n\n Conversor hex/dec/oct/bin\n --------------------------------------------");
-    printf("\n\n Por favor informe a base do valor de entrada: \n [0]-Hexa  [1]-Deci  [2]-Octa  [3]-Bin");
+    printf("\n\n Por favor informe a base do valor de entrada: \n [0]-Hexa  [1]-Deci  [2]-Octa  [3]-Bin \n ");
 
     scanf_s("%d", &base);
     while (base != 0 && base != 1 && base != 2 && base != 3)
@@ -34,8 +113,7 @@ int main() {
 
     printf("\n\n Base do valor de entrada selecionada: ");
 
-    switch (base)
-    {
+    switch (base){
     case 0:
 
         printf(" Hexadecimal\n Insira o valor: ");
@@ -76,107 +154,28 @@ int main() {
     }
        
     // os fors procuram o digito de parada descrito na função de leitura, que é "-1".
+    // A função getc retorna valores ASCII, e as funções de print printam inteiros, o "- '0' " serve para tornar carácteres ASCII em inteiros.
+
+
     if (dec != NULL) {
         printf("\n dec : ");
         for (size_t i = 0; dec[i] != -1; i++)
         {
-            printf("%d", dec[i]);
+            printf("%d", dec[i] - '0');
         }
     }
     if (oct != NULL) {
         printf("\n oct : ");
         for (size_t i = 0; oct[i] != -1; i++)
         {
-            printf("%d", oct[i]);
+            printf("%d", oct[i] - '0');
         }
     }
     if (bin != NULL) {
         printf("\n bin : ");
         for (size_t i = 0; bin[i] != -1; i++)
         {
-            printf("%d", bin[i]);
+            printf("%d", bin[i] - '0');
         }
     }
 }
-
-
-//FUNÇÕES DE LEITURA
-
-int* readInteger() {
-    FILE* file = stdin;
-    int* num;
-    size_t size = 2, count = 0; //tamanho default e contagem para array
-
-    num = malloc(sizeof(*num) * size); // aloca memória ao num de tamanho *num (int) vezes size (2)
-
-    while (EOF != (num[count] = fgetc(file)) && num[count++] != '\n') {
-        /*  EOF != (num[count] = fgetc(stdin))
-            lê o número fgetc(stdin) e o coloca
-            dentro da array num[count], depois
-            confirma que não é EOF.*/
-
-            /*  num[count++] != '\n'
-                compara o número lido com '\n',
-                e depois incrementa count. */
-
-        if (count + 1 == size) {
-            /*  vê se a contagem bateu perto do
-                limite da array, se sim realoca
-                mais espaço para ser utilizado:*/
-
-            num = realloc(num, sizeof(*num) * (size += 1));
-            /*  size+=1 incrementa size e depois faz a
-                operação.*/
-        }
-    }
-    // se a condição do while é falsa, num[count] = '\n' ou EOF.
-
-    // portanto deve ser retornado, com tamanho [count]:
-    // o último digito deve ser -1, para servir de parada na hora de printar
-    num[count] = -1; 
-    return realloc(num, sizeof(*num) * count);
-
-    //retorna um ponteiro apontando para um vetor de inteiros.
-}
-
-char* readChar() {
-    char* num;
-    size_t size = 2, count = 0; //tamanho default e contagem para array
-
-    num = malloc(sizeof(*num) * size); // aloca memória ao num de tamanho *num (int) vezes size (2)
-
-    while (EOF != (num[count] = fgetc(stdin)) && num[count++] != '\n') {
-        /*  EOF != (num[count] = fgetc(stdin))
-            lê o número fgetc(stdin) e o coloca
-            dentro da array num[count], depois
-            confirma que não é EOF.*/
-
-            /*  num[count++] != '\n'
-                compara o número lido com '\n',
-                e depois incrementa count. */
-
-        if (count + 1 == size) {
-            /*  vê se a contagem bateu perto do
-                limite da array, se sim realoca
-                mais espaço para ser utilizado:*/
-
-            num = realloc(num, sizeof(*num) * (size += 1));
-            /*  size+=1 incrementa size e depois faz a
-                operação.*/
-        }
-    }
-
-    // se a condição do while é falsa, num[count] = '\n' ou EOF.
-
-    // portanto deve ser retornado, com tamanho [count]:    
-    num[count] = '\n';
-    return realloc(num, sizeof(*num) * count);
-
-    // retorna um ponteiro apontando para um vetor de caracteres.
-}
-
-//FUNÇÕES DE CONVERSÃO
-
-
-
-//FUNÇÕES MATEMÁTICAS
