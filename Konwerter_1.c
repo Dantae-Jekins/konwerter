84d30bef89ece8ea6b2dee55c8052c60c9b89921
Dantae <dantae.sona.jekins@gmail.com>
2021-05-16 14:45:22 -0300

    Dec to bin and oct working fine, finishing reverse flags now.

--- Konwerter_1.c
+++ Konwerter_1.c
@@ -1,30 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 //  Declaração das funções (para o compilador não assumir errado);
 //  Suponho que funcionaria se eu colocasse as funções antes de main também.
 //  eu tõ exagerando nos comentários para eu mesmo montar o programa
 //FUNÇÕES DE LEITURA
 
-int *readInteger() {
-    FILE* file = stdin;
-    int* num;
+int *readInteger()
+{
+    FILE *file = stdin;
+    int *num;
     size_t size = 2, count = 0; //tamanho default e contagem para array
 
     num = malloc(sizeof(int) * size); // aloca memória ao num de tamanho *num (int) vezes size (2)
 
-    while (EOF != (num[count] = fgetc(file)) && num[count] != '\n') {
+    while (EOF != (num[count] = fgetc(file)) && num[count] != '\n')
+    {
         /*  EOF != (num[count] = fgetc(stdin))
             lê o número fgetc(stdin) e o coloca
             dentro da array num[count], depois
             confirma que não é EOF.*/
 
-            /*  num[count++] != '\n'
+        /*  num[count++] != '\n'
                 compara o número lido com '\n',
                 e depois incrementa count. */
 
-        if (count + 1 == size) {
+        if (count + 1 == size)
+        {
             /*  vê se a contagem bateu perto do
                 limite da array, se sim realoca
                 mais espaço para ser utilizado:*/
@@ -40,30 +42,33 @@ int *readInteger() {
 
     // portanto deve ser retornado, com tamanho [count]:
     // o último digito deve ser -1, para servir de parada na hora de printar
-    num[count] = -1; 
+    num[count] = -1;
     return num;
 
     //retorna um ponteiro apontando para um vetor de inteiros.
 }
 
-char *readChar() {
-    FILE* file = stdin;
-    char* num;
+char *readChar()
+{
+    FILE *file = stdin;
+    char *num;
     size_t size = 2, count = 0; //tamanho default e contagem para array
 
     num = malloc(sizeof(char) * size); // aloca memória ao num de tamanho *num (int) vezes size (2)
 
-    while (EOF != (num[count] = fgetc(file)) && num[count] != '\n') {
+    while (EOF != (num[count] = fgetc(file)) && num[count] != '\n')
+    {
         /*  EOF != (num[count] = fgetc(stdin))
             lê o número fgetc(stdin) e o coloca
             dentro da array num[count], depois
             confirma que não é EOF.*/
 
-            /*  num[count++] != '\n'
+        /*  num[count++] != '\n'
                 compara o número lido com '\n',
                 e depois incrementa count. */
 
-        if (count + 1 == size) {
+        if (count + 1 == size)
+        {
             /*  vê se a contagem bateu perto do
                 limite da array, se sim realoca
                 mais espaço para ser utilizado:*/
@@ -78,141 +83,197 @@ char *readChar() {
 
     // se a condição do while é falsa, num[count] = '\n' ou EOF.
 
-    // portanto deve ser retornado, com tamanho [count]:    
+    // portanto deve ser retornado, com tamanho [count]:
     num[count] = '\0';
     return num;
 
     // retorna um ponteiro apontando para um vetor de caracteres.
 }
 
+////FUNÇÕES MATEMÁTICAS
+
+int arraySum(int *array, int size)
+{
+    int sum = 0;
+    for (size_t i = 1, j = size-1; j > 0; i *= 10, j--, size--) 
+        //j deve ser carregado com size-1, pois senão ele conta a ordem de parada (-1)
+        sum += array[j - 1] * i;
+    return sum;
+}
+
 //FUNÇÕES DE CONVERSÃO
 
-int *decBin(int flag, int input[]) {
+int *decBin(int flag, int input[])
+{
 
     int *output;
     int size = 1;
+    int dec;
+    int sys = 2;
     //!primeiro extrair o número em si da array absorvida em input reversamente:
 
     //achar o tamanho da array
     //primeira tentativa foi utilizar sizeof(array), porem o compilador não compreende esta função com ponteiros,
     //por isso é necessário achar o tamanho através de uma contagem até -1, que é a ordem de parada
     //por causa da ordem de parada é necessário que size conte 1 a mais, para que represente o tamanho real
-    while(input[size-1]!=-1)
-    size++;
+    while (input[size - 1] != -1)
+        size++;
 
-    if (flag == 0) { 
+    if (flag == 0)
+    {
         //flag = 0 ; dec -> bin
-
-        //
-        
+        //primeiro nós devemos extrair o inteiro total da array, da seguinte forma:
+        dec = arraySum(input, size);
+        size = 0;
+        //com  o número inteiro, nós somos capazes de realizar a matemática para extrair o valor em binário :)
+        int carry = dec;
+        while (carry >= sys)
+        {
+            //primeiramente deve-se achar o tamanho que output terá
+            carry /= sys;
+            size++;
+        }
+        //após ter conhecimento disso alocamos um espaço
+        output = malloc(sizeof(int) * (size + 1));
+        output[size+1]=-1;//ordem de parada
+        //e então adicionamos os valores para output reversamente, ou seja, os restos das divisões.
+        for (carry = dec; size >= 0; size--)
+        {
+            output[size] = carry % sys;
+            carry /= sys;
+        }
     }
-    else if (flag == 1) { 
+    else if (flag == 1)
+    {
         //flag = 1 ; bin -> dec
 
         // aloca um espaço para um ponteiro chamado de carry
-        int *carry = malloc(sizeof(int)*size);
+        int *carry = malloc(sizeof(int) * size);
         //então podemos extrair o reverso de input, que será utilizado para extrair o valor em decimal
 
-        for( int i = size-1, j = 0; i>0; i--, j++)
+        for (int i = size - 1, j = 0; i > 0; i--, j++)
 
             //passa array[4,3,2,1,0] para carry[0,1,2,3,4]
-            carry[j] = input[i-1];
-            //Input tem que ser i-1 porque o último elemento de input(-1) não deve ser transferido.
+            carry[j] = input[i - 1];
+        //Input tem que ser i-1 porque o último elemento de input(-1) não deve ser transferido.
 
-        carry[size-1]=-1; // adiciona ordem de parada em carry :I..
-
+        carry[size - 1] = -1; // adiciona ordem de parada em carry :I..
     }
-    else {
+    else
+    {
         printf("ERROR, função binOct FLAG INVÁLIDA");
     }
 
     return output;
 }
 
-int *decOct(int flag, int *input) {
-
+int *decOct(int flag, int *input)
+{
     int *output;
     int size = 1;
+    int dec;
+    int sys = 8;
+    //!primeiro extrair o número em si da array absorvida em input reversamente:
 
-    while(input[size-1]!=-1)
+    //achar o tamanho da array
+    //primeira tentativa foi utilizar sizeof(array), porem o compilador não compreende esta função com ponteiros,
+    //por isso é necessário achar o tamanho através de uma contagem até -1, que é a ordem de parada
+    //por causa da ordem de parada é necessário que size conte 1 a mais, para que represente o tamanho real
+    while (input[size - 1] != -1)
         size++;
 
-    if (flag == 0) {      
-        //flag = 0 ; dec -> oct
-    
+    if (flag == 0)
+    {
+        //flag = 0 ; dec -> bin
+        //primeiro nós devemos extrair o inteiro total da array, da seguinte forma:
+        dec = arraySum(input, size);
+        size = 0;
+        //com  o número inteiro, nós somos capazes de realizar a matemática para extrair o valor em binário :)
+        int carry = dec;
+        while (carry >= sys)
+        {
+            //primeiramente deve-se achar o tamanho que output terá
+            carry /= sys;
+            size++;
+        }
+        //após ter conhecimento disso alocamos um espaço
+        output = malloc(sizeof(int) * (size + 1));
+        output[size+1]=-1;//ordem de parada
+        //e então adicionamos os valores para output reversamente, ou seja, os restos das divisões.
+        for (carry = dec; size >= 0; size--)
+        {
+            output[size] = carry % sys;
+            carry /= sys;
+        }
     }
-    else if (flag == 1) { 
+    else if (flag == 1)
+    {
         //flag = 1 ; oct -> dec
-        int *carry = malloc(sizeof(int)*size);
-        for( int i = size-1, j = 0; i>0; i--, j++)
-            carry[j] = input[i-1];
-        carry[size-1]=-1;    
+        int *carry = malloc(sizeof(int) * size);
+        for (int i = size - 1, j = 0; i > 0; i--, j++)
+            carry[j] = input[i - 1];
+        carry[size - 1] = -1;
     }
-    else {
+    else
+    {
         printf("ERROR, função binOct FLAG INVÁLIDA");
     }
 
     return output;
 }
 
-int *binOct(int flag, int *input) { 
+int *binOct(int flag, int *input)
+{
 
-    if (flag == 0) {
+    if (flag == 0)
+    {
         //flag = 0 ; bin -> oct
         int *carry = decBin(1, input);
         return decOct(0, carry);
     }
-    else if(flag == 1){
+    else if (flag == 1)
+    {
         //flag = 1 ; oct -> bin
         int *carry = decOct(1, input);
         return decBin(0, carry);
     }
-    else {
+    else
+    {
         printf("ERROR, função binOct FLAG INVÁLIDA");
     }
-
 }
-
 
 // hexadecimais tem que receber um tratamento especial :I
-int hexInput(char input, int b) {
+int hexInput(char input, int b)
+{
     //deve retornar um tipo int
 }
-char hexOutput(int input, int b) {
+char hexOutput(int input, int b)
+{
     //deve retornar um tipo char
-
-}
-
-
-
-
-//FUNÇÕES MATEMÁTICAS
-
-int arraySum(int array[], int size){
-    int sum = 0;
-    for (size_t i = 1, j = size; j > 0; i*=10, j--, size--)
-        sum += array[j-1]*i; 
-    return sum;
 }
 
 //clear input
-void clearinput(void) {
+void clearinput(void)
+{
     while (getchar() != '\n');
 }
 
 // main
 
-void main() {
+void main()
+{
     int base;
-    int *bin=NULL, *oct=NULL, *dec=NULL;
-    char *hex=NULL;
+    int *bin = NULL, *oct = NULL, *dec = NULL;
+    char *hex = NULL;
 
     printf("\n\n Conversor hex/dec/oct/bin\n --------------------------------------------");
     printf("\n\n Por favor informe a base do valor de entrada: \n [0]-Hexa  [1]-Deci  [2]-Octa  [3]-Bin \n ");
 
-    do {
+    do
+    {
         scanf("%d", &base);
-        if(base != 0 && base != 1 && base != 2 && base != 3)
+        if (base != 0 && base != 1 && base != 2 && base != 3)
             printf("\n Este número não corresponde a uma base, insira um correto");
 
     } while (base != 0 && base != 1 && base != 2 && base != 3);
@@ -222,14 +283,16 @@ void main() {
 
     printf("\n\n Base do valor de entrada selecionada: ");
 
-    switch (base){
+    switch (base)
+    {
     case 0:
 
         printf(" Hexadecimal\n Insira o valor: ");
 
         //lê em hexa
         hex = readChar();
-        if(hex==NULL){
+        if (hex == NULL)
+        {
             printf("ERRO HEX==NULL");
             break;
         }
@@ -244,7 +307,6 @@ void main() {
         printf("\n hex : ");
         for (size_t i = 0; hex[i] != '\0'; i++)
             printf("%c", hex[i]);
-        
 
         break;
 
@@ -254,21 +316,30 @@ void main() {
 
         //Lê em decimal
         dec = readInteger();
-        if(dec==NULL){
+        if (dec == NULL)
+        {
             printf("ERRO DEC==NULL");
             break;
-        }        
+        }
         //printa e transforma o valor decimal de ASCII para inteiros
         printf("\n dec : ");
         for (size_t i = 0; dec[i] != -1; i++)
-            printf("%d", dec[i]-='0');
-        
+            printf("%d", dec[i] -= '0');
+
         //converte em hexadecimal
 
         //converte em binário
         bin = decBin(0, dec);
+        printf("\n bin : ");
+        for (size_t i = 0; bin[i] != -1; i++)
+            printf("%d", bin[i]);
+
         //converte em octal
         oct = decOct(0, dec);
+        printf("\n oct : ");
+        for (size_t i = 0; oct[i] != -1; i++)
+            printf("%d", oct[i]);
+            
 
         break;
 
@@ -278,14 +349,15 @@ void main() {
 
         //lê em octal
         oct = readInteger();
-        if(oct==NULL){
+        if (oct == NULL)
+        {
             printf("ERRO OCT==NULL");
             break;
         }
         //printa e transforma o valor octal de ASCII para inteiros
         printf("\n oct : ");
         for (size_t i = 0; oct[i] != -1; i++)
-            printf("%d", oct[i]-='0');
+            printf("%d", oct[i] -= '0');
 
         //converte em hexadecimal
 
@@ -300,23 +372,24 @@ void main() {
     case 3:
 
         printf(" Binário\n Insira o valor: ");
-        
+
         //lê em binário
         bin = readInteger();
-        if(bin==NULL){
+        if (bin == NULL)
+        {
             printf("ERRO BIN==NULL");
             break;
         }
         //printa e transforma o valor binário de ASCII para inteiros
         printf("\n bin : ");
         for (size_t i = 0; bin[i] != -1; i++)
-            printf("%d",bin[i]-='0');
+            printf("%d", bin[i] -= '0');
 
         //converte em hexadecimal
 
         //converte em decimal
         dec = decBin(1, bin);
-        
+
         //converte em octal
         //oct = binOct(0, bin);
 
